af680a6f021476747b9b72781a651bee68ef18cc
Nicolas GRENECHE <n.greneche45@gmail.com>
2020-05-04 11:28:54 +0200

    fin de l'algo de tri

--- tri.py
+++ tri.py
@@ -1,3 +1,4 @@
+
 ListeATrier = []
 
 rep = True
@@ -34,6 +35,13 @@ def debut():
         Bulle(ListeATrier)
     return
 
+
+#======================================================
+# 
+#      TRI EN UTILISANT LES FONCTIONS PYTHON
+#
+#======================================================
+
 def Fct(mylist):
     print(" => Fonctions Python !!")
     print("")
@@ -42,12 +50,53 @@ def Fct(mylist):
     print("Après : {}".format(mylist))
     return
 
+
+
+#======================================================
+# 
+#      TRI EN UTILISANT LA METHODE DE SELECTION
+#
+#======================================================
+
 def Select(mylist):
-    print("Methode Selection !!")
+    print(" => Methode Selection !!")
+    print("")
+    print("Avant : {}".format(mylist))
+
+    for i in range(0 , len(mylist)):
+        max = i
+        for j in range(i + 1 , len(mylist)):
+            if mylist[j] > mylist[max]:
+                max = j
+        if max != i:
+            temp = mylist[i]
+            mylist[i] = mylist[max]
+            mylist[max] = temp
+
+    print("Après : {}".format(mylist))
     return
 
+
+
+
+#======================================================
+# 
+#      TRI EN UTILISANT LA METHODE BULLE
+#
+#======================================================
 def Bulle(mylist):
-    print("Methode Bulle !!")
+    print(" => Methode Bulle !!")
+    print("")
+    print("Avant : {}".format(mylist))
+
+    for i in range(len(mylist) - 1, -1 , -1):
+        for j in range(0 , i):
+            if mylist[j+1] > mylist[j]:
+                temp = mylist[j+1]
+                mylist[j+1] = mylist[j]
+                mylist[j] = temp
+
+    print("Après : {}".format(mylist))
     return
         
 
